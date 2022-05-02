7376f861ef8c32e5aba157087c984e80bceb7cc2
Vaent <gh@vaent.uk>
2022-05-02 10:42:23 +0100

    Add a gallery option to include nude art
    Nude images are by default not included, users must actively select the
    option if they wish to see those images. User selection is currently not
    stored - the page will default to "No" on every load.

--- www/gallery/index.php
+++ www/gallery/index.php
@@ -4,12 +4,11 @@
         <meta charset="utf-8">
         <title>Gallery</title>
         <link rel="stylesheet" href="/styles/default.css">
+        <link rel="stylesheet" href="/styles/components.css">
         <link rel="stylesheet" href="/styles/gallery.css">
+        <script src="/scripts/toggles.js"></script>
     </head>
     <body>
-        <div style="text-align:center;padding:30px">
-            This page is still in development. More images and options are on the way!
-        </div>
         <div class="image-group">
             <?php
                 $conf = parse_ini_file('../../conf.ini');
@@ -23,24 +22,48 @@
                 foreach ($images as $image) {
                     $filename = "{$image['image_name']}.png";
                     $thumb = "{$image['image_name']}-thumb.png";
-                    $src = $onclick = null;
+                    $src = $onclick = $isNude = $display = null;
 
                     if (file_exists("images/thumbs/$thumb")) {
-                        $src = "images/thumbs/$thumb";
+                        $src = "src=images/thumbs/$thumb";
                     }
                     if (file_exists("images/$filename")) {
-                        $onclick = " onclick=\"expandImage('images/$filename')\"";
-                        $src ??= "images/$filename";
+                        $onclick = "onclick=\"expandImage('images/$filename')\"";
+                        $src ??= "src=images/$filename";
                     }
                     if ($src) {
+                        if ($image["is_nude"]) {
+                            $isNude = "data-is-nude=\"true\"";
+                            $src = "data-$src";
+                            $display = "style=\"display:none\"";
+                        }
                         echo <<<THUMB
-                        <span class="thumbnail">
-                            <img src="$src" alt="{$image['image_description']}"$onclick>
+                        <span class="thumbnail" $display>
+                            <img alt="{$image['image_description']}" $src $onclick $isNude>
                         </span>
                         THUMB;
                     }
                 }
             ?>
+        </div>
+        <div id="gallery-settings">
+            <h1>Gallery settings</h1>
+            <hr>
+            <p>Include nudes?
+                <span class="toggle-selector">
+                    <span data-toggle-position="0" class="toggle-currently-selected">No</span><!--
+                 --><span class="toggle-slider" data-currently-selected="0" onclick="toggleNudes(this)"></span><!--
+                 --><span data-toggle-position="1" class="toggle-not-selected" onclick="toggleNudes(this)">Yes</span>
+                </span>
+            </p>
+            <p style="font-style:italic">
+                By default, images which prominently feature human nudity are not displayed or downloaded to your machine;
+                so anyone who doesn't want to see that content, doesn't have to.
+            </p>
+            <p style="font-style:italic">
+                That said, the nude art in this collection is no more "scandalous" than what you might see in a public gallery/museum.
+                If you're comfortable with that, you can use the toggle to update the page with those images.
+            </p>
         </div>
 
         <div id="expanded-image-overlay" onclick="collapseImage()">
---
+++ www/scripts/toggles.js
@@ -0,0 +1,34 @@
+/* source: github.com/Vaent/image-gallery */
+"use strict";
+
+function toggle(elementClicked) {
+    if (elementClicked.classList.contains("toggle-slider") || elementClicked.classList.contains("toggle-not-selected")) {
+        const siblings = Array.from(elementClicked.parentElement.children);
+        const slider = siblings.find(s => s.classList.contains("toggle-slider"));
+        const oldPosition = slider.dataset.currentlySelected;
+        const newPosition = 1 ^ oldPosition;
+        slider.dataset.currentlySelected = newPosition;
+        const oldSelectedElement = siblings.find(s => s.dataset.togglePosition == oldPosition);
+        const newSelectedElement = siblings.find(s => s.dataset.togglePosition == newPosition);
+        oldSelectedElement.classList.replace("toggle-currently-selected", "toggle-not-selected");
+        newSelectedElement.classList.replace("toggle-not-selected", "toggle-currently-selected");
+        oldSelectedElement.onclick = newSelectedElement.onclick;
+        newSelectedElement.onclick = null;
+        return true;
+    }
+}
+
+function toggleNudes(elementClicked) {
+    if (toggle(elementClicked)) {
+        const isSelectedShowNude = elementClicked.parentElement.getElementsByClassName("toggle-slider")[0].dataset.currentlySelected;
+        for (let thumb of document.getElementsByClassName("thumbnail")) {
+            if (isSelectedShowNude == true) {
+                thumb.firstElementChild.src ||= thumb.firstElementChild.dataset.src;
+                thumb.style.display = "";
+            } else if (thumb.firstElementChild.dataset.isNude) {
+                thumb.style.display = "none";
+            }
+        }
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+}
